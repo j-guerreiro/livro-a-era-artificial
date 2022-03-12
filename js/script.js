7a0f00b203339e04387653e08558d6cfbdfd2c3a
j-guerreiro <joel.borghi@gmail.com>
2022-03-12 19:48:28 -0300

    update - refatorando css, conteineres flex, rodape, menu topo

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* menu abre e fecha */
+/* em desenvolvimento */
+const abreMenu = () => {
+  let menu = document.querySelector(".menu-principal");
+  let menuItens = document.querySelector(".elementos-menu");
+}
+/* em desenvolvimento */
